e06152926e9a6f2f13d8edff78a43b91d8c3390b
iDrack <tdignoire@protonmail.com>
2020-11-20 12:42:43 +0100

    Added function to modify matrixes

--- main.c
+++ main.c
@@ -1,5 +1,81 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define TAILLE 10
+
+typedef enum etat{ATTENTE,PLACEE,TOUCHE,COULE}Etat;
+typedef enum navireType{PORTEAVION,CROISER,DESTROYER,SOUSMARIN,TORPILLEUR}NavireType;
+
+typedef struct matrice{
+    int taille; //Taille de notre matrice
+    char value[TAILLE][TAILLE]; //Notre matrice
+    char *titre;
+}Matrice;
+
+typedef struct navire{
+    NavireType nom;
+    Etat etat;
+    Matrice *matrice;
+    int taille;
+    //Ajouter les tris speciaux
+}Navire;
+
+Navire *genererNavire(NavireType nt, Matrice *m){
+    Navire *n = malloc(sizeof(Navire));
+    n->nom=nt;
+    n->etat=ATTENTE;
+    n->matrice=m;
+    switch (nt){
+    case PORTEAVION:
+        n->taille=5;
+        break;
+    case CROISER:
+        n->taille=4;
+        break;
+    case DESTROYER:
+        n->taille=3;
+        break;
+    case SOUSMARIN:
+        n->taille=3;
+        break;
+    case TORPILLEUR:
+        n->taille=2;
+        break;
+    
+    default:
+        break;
+    }
+    return n;
+}
+
+Matrice* genererMatriceVide(char *titre){
+    //Genere une matrice vide
+    Matrice *m = malloc(sizeof(Matrice));
+    m->titre=titre;
+    m->taille=TAILLE;
+    for(int i=0;i<m->taille;i++){
+        for(int j=0;j<m->taille;j++){
+            m->value[i][j]='.';
+        }
+    }
+    return m;
+}
+
+void afficherMatrice(Matrice *m){
+    printf("%s:\n ",m->titre);
+    for(int i=0;i<m->taille;i++)printf(" %c",65+i);
+    printf("\n");
+    for(int i=0;i<m->taille;i++){
+        printf("%d",i);
+        for(int j=0;j<m->taille;j++){
+            printf(" %c",m->value[i][j]);
+        }
+        printf("\n");
+    }
+}
 
 int main(){
-
+    Matrice *m=genererMatriceVide("Matrice du joueur");
+    afficherMatrice(m);
+    return 0;
 }
