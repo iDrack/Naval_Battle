44e212051c0c96990f643aa32021d132ad59c8a9
SpartanCharles <spartancharles1337@yahoo.fr>
2020-11-29 12:25:04 +0100

    Up.

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main(){
     Navire *armadaJoueur[TAILLE_FLOTTE];
     Navire *armadaAdversaire[TAILLE_FLOTTE];
     int taille_matrice = 10, choix = 0;
-/*
+
     // ----- Menu Principal -----
 
     printf("\nBienvenue dans la bataille navale.\n\n");
@@ -34,15 +34,13 @@ int main(){
     afficherArmada(armadaJoueur);
 
     // ---- Fin Menu Principal ----
-*/
 
+/*
     // -------- Zone Test --------
 
-    int toucheJoueur; // Variable � mettre � 0 de base !
-    int joueurTirSpecial; // Idem, � mettre � 0.
+    int toucheJoueur; // Variable : 0 de base !
+    int joueurTirSpecial; // Idem, 0.
 
-    // -> ".. � condition qu'au tour pr�c�dent il ait touch� un bateau .." --> variable toucheJoueur = 1
-    // --> ".. et qu'il n'ait pas utilis� de tir sp�cial " --> variable joueurTirSpecial = 0
     // Imaginons que le joueur a touch� au dernier tour jouer, alors toucheJoueur = 1.
     toucheJoueur = 1;
     // Imaginons qu'il n'a pas utilis� de tir sp�cial au dernier tour jouer, alors joueurTirSpecial = 0;
@@ -60,11 +58,9 @@ int main(){
     afficherArmada(armadaJoueur);
 
     printf("Apres : toucheJoueur : %d && joueurTirSpecial : %d \n", toucheJoueur, joueurTirSpecial);
-    // NOTE :
-    // -> cf 'x' et '+' � r�gler.
 
     // -------- Zone Test --------
-
+*/
 
     return EXIT_SUCCESS;
 }
