141556b86d7309ca49379e35d98d1081c1aa1f30
iDrack <tdignoire@protonmail.com>
2020-11-30 17:06:39 +0100

    Fixed some typo & added explanations

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+// Réalisé par Charles Kempa & Thomas Dignoire
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -13,50 +14,57 @@ int main(){
     // ----- Menu Principal -----
 
     printf("\nBienvenue dans la bataille navale.\n\n");
-    choisirTaille(&taille_matrice);
+    choisirTaille(&taille_matrice); //Choix de la taille du champs de bataille.
+    // Initialisation des matrice
     Matrice *matriceAdversaire = genererMatriceVide("Matrice de l'adversaire", taille_matrice);
     Matrice *matriceIntermediaire = genererMatriceVide("Matrice de l'adversaire", taille_matrice);
     Matrice *matriceJoueur = genererMatriceVide("Matrice du joueur", taille_matrice);
+    // Laisse le choix au joueur de creer sa flotte ou non.
     while(choix < 1 || choix > 2){
         printf("Voulez-vous créer votre armada ?\n1. Oui\n2. Non\n>");
         scanf("%d",&choix);
         printf("\n");
     }
-
     if(choix == 1){
-        genererArmadaJoueur(matriceJoueur,armadaJoueur); //Quand le joueur veut choisir ses navires
+        genererArmadaJoueur(matriceJoueur,armadaJoueur); // Quand le joueur veut choisir ses navires.
     }else if(choix==2){
-        placementAleatoire(matriceJoueur, armadaJoueur); //Quand le joueur ne place pas lui meme ses navires
+        placementAleatoire(matriceJoueur, armadaJoueur); // Quand le joueur ne place pas lui meme ses navires.
     }
-    placementAleatoire(matriceAdversaire, armadaAdversaire); //Generation de la grille de l'adversaire
+    placementAleatoire(matriceAdversaire, armadaAdversaire); // Generation de la grille de l'adversaire.
 
     // ---- Fin Menu Principal ----
 
     // ----- Boucle Principal -----
-
+    // La partie s'arrête une fois qu'un joueur n'a plus de flotte.
     while(nbNaviresCoulees(armadaJoueur) != 5 && nbNaviresCoulees(armadaAdversaire) != 5){
-        //Le joueur joue sur les tours impairs car il commence
+        // Le joueur joue sur les tours impairs car il commence.
         printf("\033[0;36mTour n°%d, \033[0m",tour);
         if(tour % 2 == 1){
+            // Durant le tour du joueur, on affiche sa grille, sa flotte ainsi que ses tirs disponibles.
             printf("\033[0;36mjoueur.\033[0m\n\n");
             afficherMatrice(matriceJoueur);
             afficherArmada(armadaJoueur);
             printf("Nombre de navires ennemis restants:\033[0;36m% d\033[0m\n\n",(5-nbNaviresCoulees(armadaAdversaire)));
+            // Ici, on demande au joueur le tir qu'il veut faire ainsi que les coordonnées.
             effectuerTir(matriceAdversaire, matriceIntermediaire, armadaJoueur, armadaAdversaire, &toucheJoueur, &joueurTirSpecial);
         }else{
+            // Durant le tour de l'IA, on affiche la matrice intermédiaire, une matrice où le joueur peut voir là où il a tiré mais il ne verra pas les bateaux adverses.
+            // Puis on fait jouer l'automate.
             printf("\033[0;36mordi.\033[0m\n\n");
             afficherMatrice(matriceIntermediaire);
         }
         puts("-------------------------------------------------------------------------------------------");
-        if(tour==20)break;//A retirer apres les tests
+        // On augmente le nombre de tours.
         tour++;
     }
     if(nbNaviresCoulees(armadaAdversaire) == 5){
+        // Si l'IA n'a plus de navire, le joueur gagne.
         printf("\n\033[0;33mFélicitation Joueur, vous avez gagné !\033[0m\n");
     }else if(nbNaviresCoulees(armadaJoueur) == 5){
+        // Si le joueur n'a plus de navire, l'IA gagne.
         printf("\n\033[0;31mMalheureusement votre flotte a été détruite. Défaite.\033[0m\n");
     }
-    //Libération de la mémoire
+    // Libération de la mémoire
     free(matriceJoueur);
     free(matriceAdversaire);
     
